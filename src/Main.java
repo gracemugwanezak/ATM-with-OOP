b3500889eac61a5dca7adae43153a05d6933b00a
Grace MUGWANEZA <gracemugwaneza018@gmail.com>
2025-09-17 15:09:10 +0200

    display balance with all 3 methods

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
  Atm myObj= new Atm();
         double deposit = myObj.deposit();
-  double secondDeposit= myObj.deposit();
-   double thirdDeposit=  myObj.deposit();
+  String secondDeposit= myObj.deposit( "ch32",300);
+   double thirdDeposit=  myObj.deposit("", 4000.0);
     }
 }
